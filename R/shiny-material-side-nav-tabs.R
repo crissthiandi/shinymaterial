bd1e0aedfe92e01961c52b2bb40ddc059373e7d1
crissthiandi <albertocenaa@gmail.com>
2025-05-18 13:36:14 -0600

    feat: add support to see which tab is actived

--- R/shiny-material-side-nav-tabs.R
+++ R/shiny-material-side-nav-tabs.R
@@ -76,7 +76,8 @@ material_side_nav_tabs <- function(side_nav_tabs, icons = NULL, color = NULL, fo
             "$('#", side_nav_tabs[[i]], "').trigger('show');",
             "$('#", side_nav_tabs[[i]], "').trigger('shown');",
             "$('#", paste0(side_nav_tabs[[i]], "_tab_id"), "').addClass('active');",
-            "$('#side_nav_tabs_click_info').trigger('click');"
+            "$('#side_nav_tabs_click_info').trigger('click');",
+            "Shiny.setInputValue('sidebar_tab_js', '",side_nav_tabs[[i]],"');"
           ),
           icon_tag.i, names(side_nav_tabs)[[i]]
         )
